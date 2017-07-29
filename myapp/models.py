4bf3288d179dbe6f7eebcef80517735e4297a20a
Prachi Lepcha <prachi06lepcha@gmail.com>
2017-07-29 21:51:16 +0530

    like comment

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+
 import uuid
 
-class UserModel(models.Model):
+# Create your models here.
+
+
+class UserModel(models.Model):			#Model for user
 	email = models.EmailField()
 	name = models.CharField(max_length=120)
 	username = models.CharField(max_length=120)
@@ -9,7 +13,7 @@ class UserModel(models.Model):
 	created_on = models.DateTimeField(auto_now_add=True)
 	updated_on = models.DateTimeField(auto_now=True)
 
-class SessionToken(models.Model):
+class SessionToken(models.Model):		#Model for session token
 	user = models.ForeignKey(UserModel)
 	session_token = models.CharField(max_length=255)
 	last_request_on = models.DateTimeField(auto_now=True)
@@ -19,17 +23,44 @@ class SessionToken(models.Model):
 	def create_token(self):
 		self.session_token = uuid.uuid4()
 
+class PostModel(models.Model):			#Model for post
+	user = models.ForeignKey(UserModel)
+	image = models.FileField(upload_to='user_images')
+	image_url = models.CharField(max_length=255)
+	caption = models.CharField(max_length=240)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+	has_liked = False
 
-class LikeModel(models.Model):
+
+	@property
+	def like_count(self):
+		return len(LikeModel.objects.filter(post=self))
+
+	@property
+	def comments(self):
+		return CommentModel.objects.filter(post=self).order_by('-created_on')
+
+class LikeModel(models.Model):			#Model for like
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+
+class CommentModel(models.Model):		#Model for comment
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	comment_text = models.CharField(max_length=555)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(CommentModel)
     user = models.ForeignKey(UserModel)
-    post = models.ForeignKey(PostModel)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True
+    updated_on = models.DateTimeField(auto_now=True)
 
-
-def like_view(request):
-	user = check_validation(request)
-	if user and request.method == 'POST':
-		print 'The user is valid'
-	else:
-		return redirect('/login/')
+class Search(models.Model):
+    category = models.CharField(max_length=30)
