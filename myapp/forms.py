9191ea9a0fac652a53ac36d40ab0bc902601c8dc
Prachi Lepcha <prachi06lepcha@gmail.com>
2017-08-01 17:23:09 +0530

    like comment

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from models import UserModel, PostModel, LikeModel, CommentModel
+from models import UserModel,PostModel,LikeModel,CommentModel
 
 
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = UserModel
-        fields=['Email','Username','Name','Password']
+        fields=['email','username','name','password']
 
 class LoginForm(forms.ModelForm):
     class Meta:
@@ -14,19 +14,22 @@ class LoginForm(forms.ModelForm):
 
 class PostForm(forms.ModelForm):
     class Meta:
-        model = PostModel
-        fields=['image', 'caption']
-
+        model=PostModel
+        fields=['image','caption']
 
 class LikeForm(forms.ModelForm):
-
     class Meta:
         model = LikeModel
         fields=['post']
 
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = CommentModel
-        fields = ['comment_text', 'post']
+        fields = ['comment_text', 'post','id','upvote_num']
+
+class UpvoteForm(forms.Form):
+    id = forms.IntegerField()
+
+class SearchForm(forms.Form):
+    searchquery = forms.CharField();
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,13 @@
+from __future__ import unicode_literals
 from django.shortcuts import render, redirect
-from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm, UpvoteForm, SearchForm
 from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
 from django.contrib.auth.hashers import make_password, check_password
+from instaclone.settings import BASE_DIR
+from imgurpython import ImgurClient
 from datetime import timedelta
 from django.utils import timezone
-from instaclone.settings import BASE_DIR
 
-from imgurpython import ImgurClient
-
-
-# Create your views here.
 
 def signup_view(request):
     if request.method == "POST":
@@ -57,6 +55,43 @@ def login_view(request):
     return render(request, 'login.html', response_data)
 
 
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            posts = PostModel.objects.all().order_by('-created_on')
+            for post in posts:
+
+                existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
+                if existing_like:
+                    post.has_liked = True
+
+                if not existing_like:
+                    LikeModel.objects.create(post_id=post_id, user=user)
+                else:
+                    existing_like.delete()
+
+                return redirect('/feed/')
+
+
+        else:
+            return redirect('/feed/')
+
+
+    else:
+        return redirect('/login/')
+
+
+def feed_view(request):
+    user = check_validation(request)
+    if user:
+        posts = PostModel.objects.all().order_by('created_on')
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+        return redirect('/login/')
+
 def post_view(request):
     user = check_validation(request)
 
@@ -69,50 +104,17 @@ def post_view(request):
                 post = PostModel(user=user, image=image, caption=caption)
                 post.save()
 
-                path = str(BASE_DIR + post.image.url)
+                path = str(BASE_DIR + "\\" + post.image.url)
 
-                client = ImgurClient('7e04f58c2a8358d', '3ec053214471ec982ed50789132c0e9c639a44e5')
-                post.image_url = client.upload_from_path(path, anon=True)['link']
+                client = ImgurClient('3885ef5f1212538', 'b607a83fb4e5d62d3e936075755e3a40eccc7326')
+                post.image_url = client.upload_from_path(path,anon=True)['link']
                 post.save()
 
                 return redirect('/feed/')
 
         else:
             form = PostForm()
-        return render(request, 'post.html', {'form': form})
-    else:
-        return redirect('/login/')
-
-
-def feed_view(request):
-    user = check_validation(request)
-    if user:
-
-        posts = PostModel.objects.all().order_by('created_on')
-
-        for post in posts:
-            existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
-            if existing_like:
-                post.has_liked = True
-
-        return render(request, 'feed.html', {'posts': posts})
-    else:
-
-        return redirect('/login/')
-
-
-def like_view(request):
-    user = check_validation(request)
-    if user and request.method == 'POST':
-        form = LikeForm(request.POST)
-        if form.is_valid():
-            post_id = form.cleaned_data.get('post').id
-            existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
-            if not existing_like:
-                LikeModel.objects.create(post_id=post_id, user=user)
-            else:
-                existing_like.delete()
-            return redirect('/feed/')
+        return render(request, 'post.html', {'form' : form})
     else:
         return redirect('/login/')
 
@@ -133,8 +135,7 @@ def comment_view(request):
         return redirect('/login')
 
 
-# For validating the session
-def check_validation(request):
+def check_validation(request):          # For validating the session
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
@@ -143,3 +144,61 @@ def check_validation(request):
                 return session.user
     else:
         return None
+
+def logout_view(request):
+
+    user = check_validation(request)
+
+    if user is not None:
+        latest_sessn = SessionToken.objects.filter(user=user).last()
+        if latest_sessn:
+            latest_sessn.delete()
+            return redirect("/signup/")
+            # how to get cookies in python to delete cookie n session
+
+
+def upvote_view(request):           # method to create upvote for comments
+    user = check_validation(request)
+    comment = None
+
+    print ("upvote view")
+    if user and request.method == 'POST':
+
+        form = UpvoteForm(request.POST)
+        if form.is_valid():
+
+            comment_id = int(form.cleaned_data.get('id'))
+
+            comment = CommentModel.objects.filter(id=comment_id).first()
+            print ("upvoted not yet")
+
+            if comment is not None:
+                # print ' unliking post'
+                print ("upvoted")
+                comment.upvote_num += 1
+                comment.save()
+                print (comment.upvote_num)
+            else:
+                print ('stupid mistake')
+                #liked_msg = 'Unliked!'
+
+        return redirect('/feed/')
+    else:
+        return redirect('/feed/')
+
+def query_based_search_view(request):
+
+    user = check_validation(request)
+    if user:
+        if request.method == "POST":
+            searchForm = SearchForm(request.POST)
+            if searchForm.is_valid():
+                print 'valid'
+                username_query = searchForm.cleaned_data.get('searchquery')
+                user_with_query = UserModel.objects.filter(username=username_query).first();
+                posts = PostModel.objects.filter(user=user_with_query)
+                return render(request, 'Feed.html',{'posts':posts})
+            else:
+                return redirect('/feed/')
+    else:
+        return redirect('/login/')
